52ba0123477e521fd82bd1b2ee8a2bd124cc7e9c
maotoshimojo <maotoshimojo@gmail.com>
2021-03-26 14:45:23 -0400

    express app setup

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://ie3-news-aggregator-305102.firebaseio.com',
+});
+
+const db = admin.firestore();
