e52ae108d8153b137f2c63764b8ea52f15a650e3
maotoshimojo <maotoshimojo@gmail.com>
2021-03-31 22:11:03 -0500

    outline next steps in comments

--- server/app.js
+++ server/app.js
@@ -1,3 +1,9 @@
+// TODO: Look into building a REST API with firebase
+// that can be accessed by the web app.
+// The express app will likely be moved into one index.js file
+// contained in a functions folder.
+// https://firebase.google.com/docs/hosting/serverless-overview
+
 const app = require('express')();
 const schedule = require('node-schedule');
 const db = require('./utils/firebase');
