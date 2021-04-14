d10dc813f1ee3e7bff96032ae35959e3f7d6e261
[Quinton Nickum] <[q.c.nickum@gmail.com]>
2021-04-13 23:32:36 -0500

    added final logo

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,12 +1,14 @@
+import { logRoles } from "@testing-library/dom";
 import React from "react";
 import {Link} from 'react-router-dom';
+import logo from "../imgs/news-snap-logo.png";
 
 class Header extends React.Component {
   render() {
     return (
       <header>
         <Link style={{ textDecoration: 'none' }} to="/">
-          <h1 className="headerStyle">News Snap</h1>
+          <img className="logo" src={logo} alt="News Snap"></img>
         </Link>
       </header>
     );
